46432ae871fb0cd91465db18280a534bf02911b3
R4fa3 <im.rafael.henrique@gmail.com>
2024-11-04 15:34:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Sol',
+    'Por que a luz do Sol é do passado?',
+    'A luz do Sol leva pouco mais de oito minutos para chegar até nós, por isso, quando observamos essa estrela, a imagem que vemos é do passado'
+)
+
+criaCartao(
+    'Grandes bolas de gás',
+    'Quais planetas apresentam apenas gases?',
+    'Júpiter, Saturno, Urano e Netuno são os maiores planetas do Sistema Solar e também os que apresentam as maiores massas, mesmo sendo formados exclusivamente por gases, esses planetas são bem menos densos que os planetas rochosos.'
+)
+
+criaCartao(
+    'Buracos negros',
+    'O que são?',
+    'Os buracos negros são formados nos estágios finais de vida de estrelas muito massivas, isto é, aquelas que apresentam pelo menos 6 vezes a massa do Sol. Quando o combustível dessas estrelas acaba, sua gravidade vence e elas sofrem um intenso colapso gravitacional, reduzindo drasticamente o seu tamanho.'
+)
+
+criaCartao(
+    'Universo',
+    'O que é ?',
+    'O universo se apresenta como um vasto cosmos em expansão, sem se preocupar com nada, nem mesmo em ter uma forma definida.'
+)
+
+criaCartao(
+    'Lua',
+    'Por que a Lua está se afastando da Terra ?',
+    'Por conta da força gravitacional lunar, a Terra sofre com protuberâncias que criam um desequilíbrio e retardam sua rotação; dessa forma, nosso planeta está perdendo energia e seu momento angular, assim, resultando no lento afastamento da Lua.'
+)
+
+criaCartao(
+    'Espaço',
+    'Por que tudo no espaço está no passado ?',
+    'Como a luz não chega a todos os lugares instantaneamente, quanto mais longe for sua origem, mais tempo ela demora para ser observável.'
+)
+
+criaCartao(
+    'Large Quasar Groups',
+    'O que é ?',
+    'É a maior estrutura do universo, formado por setenta e quatro quasares, que juntos têm quatro bilhões de anos-luz de diâmetro.'
+)
+
+criaCartao(
+    'Multiverso',
+    'O que é ?',
+    'Teoria chamada inflação eterna: logo após o Big Bang, o espaço-tempo teria se expandido em taxas diferentes em lugares diferentes, e isso teria dado origem a “universos bolha”.'
+)
+
+criaCartao(
+    'Água',
+    'É possível existir água no espaço ?',
+    'O maior reservatório de água no espaço está flutuando em torno de um um buraco negro a 12 bilhões de anos-luz de distância de nós. É uma nuvem de vapor de água que cobre centenas de anos-luz e contém 140 trilhões de vezes mais água do que os oceanos do mundo'
+)
+
+criaCartao(
+    'Estrela',
+    'Como é possível existir estrelas vindas de outros lugares ?',
+    'Elas vieram de outra galáxia, a teoria é que elas podem ter interagido com um grande buraco negro e sido “jogadas” gravitacionalmente com grande velocidade e assim foram capturadas pela Via Láctea. '
+)
